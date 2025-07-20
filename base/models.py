914c437f3cfae51381a092fa8d4c6458fbd99e19
Dolma <dolmas540@gmail.com>
2025-07-20 16:08:48 +0545

    IMS Project

---
+++ base/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=300)
+
+class Department(models.Model):
+    name = models.CharField(max_length=300)
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    stock = models.IntegerField()
+    type = models.ForeignKey(ProductType,on_delete=models.SET_NULL,null=True)
+    department = models.ManyToManyField(Department)
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=300)
+    phone = models.CharField()
+    email = models.EmailField()
+
+class Purchase(models.Model):
+    product = models.ForeignKey(ProductType,on_delete=models.SET_NULL,null=True)
+    price = models.FloatField()
+    vendor = models.ForeignKey(Vendor,on_delete=models.SET_NULL,null=True)
+    quantity = models.IntegerField()
+
+class Customer(models.Model):
+    name = models.CharField(max_length=300)
+    phone = models.CharField()
+
+class Sell(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
+    price = models.FloatField()
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True)
+    quantity = models.IntegerField()
